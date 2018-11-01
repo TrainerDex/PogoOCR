bd5367e17eacd4290c77ffcccbd0700fcbea5712
Jay Turner <jay@trainerdex.co.uk>
2018-11-01 13:04:56 +0000

    ProfileTop: Fix Newline
    Due to a debugging issue, I added too many slashes

--- PogoOCR/types.py
+++ PogoOCR/types.py
@@ -5,11 +5,11 @@ class ProfileTop(Image):
     
     @property
     def username(self):
-        return re.search(r'([A-Za-z0-9]+)\\n(?:\&|et|con|e) ?([^\\n]+)', self.text_found[0].description).group(1)
+        return re.search(r'([A-Za-z0-9]+)\n(?:\&|et|con|e) ?(.+)', self.text_found[0].description).group(1)
         
     @property
     def buddy_name(self):
-        return re.search(r'([A-Za-z0-9]+)\\n(?:\&|et|con|e) ?([^\\n]+)', self.text_found[0].description).group(2)
+        return re.search(r'([A-Za-z0-9]+)\n(?:\&|et|con|e) ?(.+)', self.text_found[0].description).group(2)
 
 class ProfileBottom(Image):
     
