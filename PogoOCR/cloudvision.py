6d02fb027eb4973376542b6d2aae75255129eb21
Jay Turner <jay@trainerdex.co.uk>
2019-01-20 17:55:15 +0000

    Give option of using image_content or image_uri

--- PogoOCR/cloudvision.py
+++ PogoOCR/cloudvision.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from google.cloud import vision
 from google.cloud.vision import types
 from google.oauth2 import service_account
-from PIL import Image
 
 credentials = service_account.Credentials.from_service_account_file(
     settings.GOOGLE_SERVICE_FILE)
@@ -13,15 +12,16 @@ class Image:
     Only to be ran from inside the Django environment
     """
     
-    def __init__(self, image_uri):
+    def __init__(self, image_content=None, image_uri=None):
         self.google = vision.ImageAnnotatorClient(credentials=credentials)
-        self.image_uri = image_uri
-    
-    @property
-    def image(self):
-        image = types.Image()
-        image.source.image_uri = self.image_uri
-        return image
+        
+        self.image = types.Image()
+        if image_content:
+            self.image.content = image_content
+        elif image_uri:
+            self.image_uri = image_uri
+        else:
+            raise AttributeError('Please define either image_uri or image_content')
     
     def get_text(self):
         print("Requesting TEXT_DETECTION from Google Cloud API. This will cost us 0.0015 USD.")
