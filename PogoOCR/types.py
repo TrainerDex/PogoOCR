666d2f2d71063000624d28e9048c789091e82630
Jay Turner <jay@trainerdex.co.uk>
2020-05-13 01:41:10 +0100

    Add debug prints

--- PogoOCR/types.py
+++ PogoOCR/types.py
@@ -13,12 +13,18 @@ class ProfileSelf(Image):
         return re.search(r'([A-Za-z0-9]+)\n(?:\&|et|con|e) ?(.+)', self.text_found[0].description).group(2)
     
     def find_stats(self):
-        left_split = self.text_found[0].description.split('TOTAL ACTIVITY')
+        try:
+            left_split = self.text_found[0].description.split('TOTAL ACTIVITY')
+        except AttributeError:
+            print('Please run .get_text() first!')
+            return
+        
         if len(left_split) == 2:
             stats_str_iter = iter([x for x in left_split[1].split('WEEKLY PROGRESS')[0].split('\n') if x])
             del left_split
             combo_stats = list(iter(lambda: tuple(islice(stats_str_iter,2)), ()))
             del stats_str_iter
+            num_stats_found = 0
             for x in combo_stats:
                 if x[0] == 'Distance Walked':
                     self.km_walked = float(x[1].strip().replace(',', '').replace('.', '').split(' ')[0])
@@ -30,6 +36,13 @@ class ProfileSelf(Image):
                     self.total_xp = int(x[1].strip().replace(',', '').replace('.', '').replace(' ', ''))
                 elif x[0] == 'Start Date:':
                     self.start_date_text = x[1].strip()
+                else:
+                    pass
+                num_stats_found += 1
+            print(f"Found {num_stats_found} stat(s)")
+            print(combo_stats)
+            del combo_stats
+            
         else:
             del left_split
 
