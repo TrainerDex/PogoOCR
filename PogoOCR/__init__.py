50f9ec897a3c67b63bb61ab45ac8fbc3ea04cd4e
Jay Turner <jay@trainerdex.co.uk>
2020-05-13 01:44:50 +0100

    Release 0.2.0

--- PogoOCR/__init__.py
+++ PogoOCR/__init__.py
@@ -1,11 +1,11 @@
 """
-PogoOCR for TL40
+PogoOCR
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 A Python tool for running OCR on Pokemon Screenshots using Google Cloud Vision
 Forked for TL40 specific purposes
 
-:copyright: (c) 2018 TrainerDex
+:copyright: (c) 2020 TrainerDex
 :licence: GNU GENERAL PUBLIC LICENSE 3.0
 
 """
@@ -13,8 +13,8 @@ Forked for TL40 specific purposes
 __title__ = 'PogoOCR'
 __author__ = 'JayTurnr'
 __licence__ = 'GNU GENERAL PUBLIC LICENSE 3.0'
-__copyright__ = '2018 TrainerDex'
-__version__ = '0.1.0'
+__copyright__ = '2020 TrainerDex'
+__version__ = '0.2.0'
 
 from .cloudvision import Image
 from .types import ProfileSelf, Badge, Pokédex
