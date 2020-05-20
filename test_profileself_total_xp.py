30edc635da48b093593ae8bdef291d39006d9fb5
Jay Turner <jay@trainerdex.co.uk>
2020-05-20 13:02:21 +0100

    Add extra tests

--- test_profileself_total_xp.py
+++ test_profileself_total_xp.py
@@ -4,6 +4,7 @@ import pytest
 def func(x):
 	foo = PogoOCR.ProfileSelf('key.json', image_uri=x)
 	foo.get_text()
+	print(foo.__dict__)
 	return foo.total_xp
 
 options = [
@@ -15,9 +16,11 @@ options = [
 	('es','https://cdn.discordapp.com/attachments/339450026076012544/712590402548662372/Screenshot_20200520-095940.jpg',68315780),
 	('zh_hant','https://cdn.discordapp.com/attachments/339450026076012544/712592915234422784/Screenshot_20200520-100930.jpg',68315780),
 	('en','https://cdn.discordapp.com/attachments/543105822629167104/709926063282847834/Screenshot_20200513-013228.jpg',68064220),
-	('pt_br','https://cdn.discordapp.com/attachments/339450026076012544/712597570421260408/Screenshot_20200520-102802.jpg',68315780)
+	('pt_br','https://cdn.discordapp.com/attachments/339450026076012544/712597570421260408/Screenshot_20200520-102802.jpg',68315780),
+	('edge_of_screen','https://cdn.discordapp.com/attachments/329751396222238722/711468207667544095/Screenshot_20200517-074014.png',5421143),
+	('xp_total_xp_issue','https://cdn.discordapp.com/attachments/329751396222238722/711961584230203402/Screenshot_20200518-162012_Pokmon_GO.jpg',9931565), # This is the issue being fixed, leaving this test incase I break it in future
 ]
 
-@pytest.mark.parametrize("language,input,expected", options)
-def test_answer(language,input,expected):
+@pytest.mark.parametrize("test_name,input,expected", options)
+def test_answer(test_name,input,expected):
 	assert func(input) == expected
