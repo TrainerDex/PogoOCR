fa07f8818af2eacbf86ba7670b5b6f6ab9a990e9
Jay Turner <jay@trainerdex.app>
2022-02-27 22:57:23 +0000

    Bump version to 0.4.0-beta

--- PogoOCR/__init__.py
+++ PogoOCR/__init__.py
@@ -13,7 +13,7 @@ __title__ = "PogoOCR"
 __author__ = "JayTurnr"
 __licence__ = "GNU GENERAL PUBLIC LICENSE 3.0"
 __copyright__ = "2021 TrainerDex"
-__version__ = "0.3.9"
+__version__ = "0.4.0-beta"
 
 from .providers.cloudvision import Screenshot  # noqa: F401
 from .client import OCRClient  # noqa: F401
