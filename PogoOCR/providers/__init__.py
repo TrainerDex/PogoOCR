f1ddea77c227283bb55f33c46eb0ab1425f58177
Jay Turner <jay@trainerdex.app>
2022-05-01 11:43:18 +0100

    Fix CloudVisionClient import

--- PogoOCR/providers/__init__.py
+++ PogoOCR/providers/__init__.py
@@ -1,7 +1,12 @@
 from enum import Enum
 
-from PogoOCR.providers.cloudvision import CloudVisionClient
 from PogoOCR.providers.tesseract import TesseractClient
+
+
+try:
+    from PogoOCR.providers.cloudvision import CloudVisionClient
+except ImportError:
+    CloudVisionClient = None
 
 
 class Providers(Enum):
